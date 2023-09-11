91606d1ee7c203601af07c0410bdbedb0706f199
penggrin12 <lock.work@inbox.ru>
2023-09-11 16:26:30 +0300

    will this work?

--- src/stupidwalletapi/base.py
+++ src/stupidwalletapi/base.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import httpx
 
 
@@ -5,13 +7,17 @@ class BaseAPI:
     def __init__(self, token: str, base_url: str):
         self.TOKEN = token
         self.BASE_URL = base_url
+        self.lock = asyncio.Lock()
         self._headers = {
             'accept': 'application/json',
             'api-key': self.TOKEN,
         }
 
     async def _make_request(self, method, url, params=None) -> dict:
-        async with httpx.AsyncClient() as client:
-            response = await client.request(method, self.BASE_URL + url, params=params, headers=self._headers)
-            print(response.status_code, response.json())
+        async with self.lock:
+            async with httpx.AsyncClient() as client:
+                response = await client.request(method, self.BASE_URL + url, params=params, headers=self._headers)
+                print(response.status_code, response.json())
+
+            await asyncio.sleep(0.42)
             return response.json()
