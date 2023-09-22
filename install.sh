03c86888b03b0312d0aaca9065938f01fc2f3df5
D3V <88152967+Nefrit9087@users.noreply.github.com>
2023-09-22 21:24:22 +0300

    ебнул установщик для линукс юзеров
    by @thencore_new

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ python -m build
 
 echo щас ставится уже жди залупа
 cd dist
-pip install stupidwalletapi-1.0.0-py3-none-any.whl --force-reinstall
+pip install stupidwalletapi-1.0.0-py3-none-any.whl --force-reinstall --break-system-packages
 cd ..
 
 echo вроде все, чекай
