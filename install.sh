7b239b7c8084b2a30812fcdd0e8a2dc35ce8232d
Marat Sibagatullin <admin@mrksbgg.ml>
2023-09-26 17:23:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd dist
 pip install stupidwalletapi-1.0.0-py3-none-any.whl --force-reinstall --break-system-packages
 cd ..
 
-echo Here you go!
+echo Here you go
 pip show stupidwalletapi
 
-echo Now you can DDoS Telegram datacenters!
+echo Now you can DDoS Telegram datacenters
