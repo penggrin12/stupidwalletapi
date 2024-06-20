37f36647b31ae4f4ebb0f4407118fb5600dddd89
Daniil <hello@faustyu.ru>
2024-06-20 20:47:09 +0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 
-echo Building...
+echo строим питхон
 python -m build
 
-echo Installing...
+echo устанавливаем фигню
 cd dist
 python -m pip install stupidwalletapi-1.0.0-py3-none-any.whl --force-reinstall
 cd ..
 
-echo Done!
+echo тепер можеш дудосит
